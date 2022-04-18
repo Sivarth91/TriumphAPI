f4233e3afcada8de7defcf2a70ab25b384aacdc1
A. Sivault <81822795+Sivarth91@users.noreply.github.com>
2022-04-18 11:29:45 +0200

    models

---
+++ models/motorcycles.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const motorcyclesSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    years: {
+        type: String,
+        required: true
+    },
+    period: {
+        type: String,
+        required: false
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: String,
+        required: false
+    }
+});
+
+const motorcycle = mongoose.model('Motorcycle', motorcyclesSchema);
+
+module.exports = motorcycle;
